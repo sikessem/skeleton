ae9d1564dff3aee431c7b60abf17de3b17c1fe3e
SIGUI Kessé Emmanuel <emmanuel@kesse.sigui.ci>
2023-10-22 20:38:02 +0100

    :safety_vest: Use PHP Insights

--- bootstrap.php
+++ bootstrap.php
@@ -1,17 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
 use Composer\Autoload\ClassLoader;
 
 return (static function (string $root = __DIR__): ClassLoader {
     $init_root = $root;
     $autoloader = null;
     do {
-        if (is_file($file = "$root/composer.json") && is_readable($file)) {
-            if (($data = @file_get_contents($file)) && ($data = @json_decode($data, true))) {
-                if (is_file($autoload_file = "$root/".($data['config']['vendor-dir'] ?? 'vendor').'/autoload.php') && is_readable($autoload_file)) {
+        $file = "{$root}/composer.json";
+        if (is_file($file) && is_readable($file)) {
+            $data = (string) file_get_contents($file);
+            $data = (array) json_decode($data, true);
+            if ($data) {
+                $autoload_file = "{$root}/".($data['config']['vendor-dir'] ?? 'vendor').'/autoload.php';
+                if (is_file($autoload_file) && is_readable($autoload_file)) {
                     $autoload_file = realpath($autoload_file);
                     if (in_array($autoload_file, get_included_files())) {
-                        throw new RuntimeException("The $autoload_file file has already been included.");
+                        throw new RuntimeException("The {$autoload_file} file has already been included.");
                     }
                     $autoloader = include_once $autoload_file;
                 }
